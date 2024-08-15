8ee20506e4f505cfe188cb7bb882d5bc663ca971
akshay <akshayrameke97@gmail.com>
2024-08-15 12:08:47 +0530

    deleting existing project to add new one

--- SpringBoot24-SpringBootMVC-CRM-CRUD-APP/src/main/java/in/ineuron/dao/ICustomerDao.java
+++
@@ -1,9 +0,0 @@
-package in.ineuron.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import in.ineuron.model.Customer;
-
-public interface ICustomerDao extends CrudRepository<Customer, Integer> {
-
-}
